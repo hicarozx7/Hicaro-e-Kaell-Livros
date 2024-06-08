7eb7f1b98c4ef9203377d6803f585353a578a5b9
hicarozx7 <hicarosousamoreira4@gmail.com>
2024-06-08 00:01:17 -0300

    script.js

--- script.js
+++ script.js
@@ -4,34 +4,34 @@ const ano = document.getElementById("ano");
 const lista = document.getElementById("lista");
 const button = document.getElementById("button");
 
-// Adicionando evento ao botão
+
 button.addEventListener("click", exibir);
 
-// Função para exibir o filme
+
 function exibir(event) {
-    // Prevenir comportamento padrão do botão
+    
     event.preventDefault();
 
-    // Obtendo os valores dos campos
+ 
     const valorDoTitulo = titulo.value;
     const valorDoDiretor = diretor.value;
     const valorDoAno = ano.value;
 
-    // Verificando se todos os campos estão preenchidos
+  
     if (valorDoTitulo && valorDoDiretor && valorDoAno) {
         adicionarFilme(valorDoTitulo, valorDoDiretor, valorDoAno);
         limparCampos();
     }
 }
 
-// Função para criar e adicionar um novo filme à lista
+
 function adicionarFilme(titulo, diretor, ano) {
     const novoFilme = document.createElement("p");
     novoFilme.textContent = `${titulo} - ${diretor} (${ano})`;
     lista.appendChild(novoFilme);
 }
 
-// Função para limpar os campos de entrada
+
 function limparCampos() {
     titulo.value = "";
     diretor.value = "";
